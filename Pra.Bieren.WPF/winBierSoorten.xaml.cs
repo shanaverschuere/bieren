44ba43380c660c05b3fb66395a25cfd781d480d6
shanaverschuere <60284785+shanaverschuere@users.noreply.github.com>
2020-10-06 21:34:55 +0200

    Initial commit

---
+++ Pra.Bieren.WPF/winBierSoorten.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Pra.Bieren.WPF
+{
+    /// <summary>
+    /// Interaction logic for winBierSoorten.xaml
+    /// </summary>
+    public partial class winBierSoorten : Window
+    {
+        public winBierSoorten()
+        {
+            InitializeComponent();
+        }       
+
+
+    }
+}
